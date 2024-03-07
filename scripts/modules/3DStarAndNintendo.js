322934f157d13cab6152c281ae1e85e2194f3320
AyyyeCastro <158392843+bobaScriptt@users.noreply.github.com>
2024-03-07 12:27:20 -0500

    V1.0.3 UX Improvements
    1. Implemented a loading screen until 3D Models are rendered.
    2. Fixed several model trigger points to better reflect the intentional positions. [Kirby]

--- scripts/modules/3DStarAndNintendo.js
+++ scripts/modules/3DStarAndNintendo.js
@@ -1,14 +1,38 @@
-
 import { Application } from "https://cdn.skypack.dev/@splinetool/runtime";
-  const starCanvas = document.getElementById('starCanvas');
-  const starApp = new Application(starCanvas);
-  starApp.load('https://prod.spline.design/vsOkDyFrbJ8vRWGv/scene.splinecode')
 
-  const starCanvas2 = document.getElementById('starCanvas2');
-  const starApp2 = new Application(starCanvas2);
-  starApp2
-  .load("https://prod.spline.design/rveDnJ6N6rlsRlGf/scene.splinecode")
-  .then(() => {
+// Get the loading screen element
+const loadingScreen = document.getElementById("load");
+
+// Array to hold all the model loading promises
+let modelPromises = [];
+
+const starCanvas = document.getElementById('starCanvas');
+const starApp = new Application(starCanvas);
+// Push the model loading promise into the array
+modelPromises.push(starApp.load('https://prod.spline.design/vsOkDyFrbJ8vRWGv/scene.splinecode'));
+
+const starCanvas2 = document.getElementById('starCanvas2');
+const starApp2 = new Application(starCanvas2);
+// Push the model loading promise into the array
+modelPromises.push(starApp2.load("https://prod.spline.design/rveDnJ6N6rlsRlGf/scene.splinecode"));
+
+//map these core IDs, and hide them until the loading state is done. Prevents web scrolling until rendering is complete.
+const elements = ['part1', 'part2', 'part3', 'part4', 'customFooter'].map(id => {
+  const element = document.getElementById(id);
+  element.style.display = 'none';
+  return element;
+});
+
+// Wait for all models to load
+Promise.all(modelPromises).then(() => {
+    setTimeout(() => {
+    // Hide the loading screen
+    loadingScreen.style.display = "none";
+    // show elements
+    elements.forEach(element => {
+      element.style.display = '';
+    });
+
     const Group = starApp2.findObjectByName("star");
 
     gsap.set(Group.scale, { x: 1, y: 1, z: 1 });
@@ -37,4 +61,17 @@ import { Application } from "https://cdn.skypack.dev/@splinetool/runtime";
         onLeaveBack: () => rotateGroup.pause() 
       }
     });
-  })
+
+    console.log('Models are loading. Will take about 10 seconds.');
+  }, 10000);
+});
+
+
+
+
+// Disable scrolling on the elements
+document.getElementById("part1").style.display = 'none';
+document.getElementById("part2").style.display = 'none';
+document.getElementById("part3").style.display = 'none';
+document.getElementById("part4").style.display = 'none';
+document.getElementById("customFooter").style.display = 'none';
--- scripts/modules/3DTextHeader.js
+++ scripts/modules/3DTextHeader.js
@@ -1,6 +1,40 @@
-
 import { Application } from "https://cdn.skypack.dev/@splinetool/runtime";
+
+// Get the loading screen element
+const loadingScreen = document.getElementById("load");
+
+// Array to hold all the model loading promises
+let modelPromises = [];
 
 const kirbyTextCanvas = document.getElementById('kirbyTextCanvas');
 const kirbyTextApp = new Application(kirbyTextCanvas);
-kirbyTextApp.load('https://prod.spline.design/eYLgTQKZtcEOjReL/scene.splinecode');
+// Push the model loading promise into the array
+modelPromises.push(kirbyTextApp.load('https://prod.spline.design/eYLgTQKZtcEOjReL/scene.splinecode'));
+
+//map these core IDs, and hide them until the loading state is done. Prevents web scrolling until rendering is complete.
+const elements = ['part1', 'part2', 'part3', 'part4', 'customFooter'].map(id => {
+   const element = document.getElementById(id);
+   element.style.display = 'none';
+   return element;
+ });
+ 
+
+// Wait for all models to load
+Promise.all(modelPromises).then(() => {
+   setTimeout(() => {
+      // Hide the loading screen
+      loadingScreen.style.display = "none";
+      // Enable scrolling on the elements
+      elements.forEach(element => {
+         element.style.display = '';
+       });
+
+      // room for scroll triggers/etc go here.
+      console.log('Models are loading. Will take about 10 seconds.');
+   }, 10000);
+});
+
+
+
+
+
